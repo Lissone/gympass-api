72ad23e3cc31333dfd0faf2d6a26109b1fc308c9
Lissone <leonardo.lissonez@gmail.com>
2024-05-16 14:07:59 -0300

    fix(repositories/prisma-users-repository): import user type from prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma';
-import { Prisma } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 
 import { UsersRepository } from '../users-repository';
 
