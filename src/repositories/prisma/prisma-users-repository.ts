ce3840c7eef92c5f2b8f86892a78a66451a1fe97
Lissone <leonardo.lissonez@gmail.com>
2024-05-16 13:59:09 -0300

    feat: add profile use case

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -4,6 +4,10 @@ import { Prisma } from '@prisma/client';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
